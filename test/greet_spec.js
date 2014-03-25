49d722ead89013aecb00e11daeb03fa76b5e242a
Durrrr <ihaku4@gmail.com>
2014-03-25 17:16:06 +0800

    test case improvement

--- test/greet_spec.js
+++ test/greet_spec.js
@@ -1,19 +1,10 @@
 var greet = require("greet");
 
 describe('greet', function() {
-    it("is a dummy success case", function() {
-        expect(1).to.eql(1);
-    });
-});
-
-describe('greet', function() {
     it("should greet a person by name", function() {
         expect(greet("name")).to.contain("hello");
         expect(greet("name")).to.contain("name");
     });
-});
-
-describe('greet', function() {
     it("should greet a person flirtatiously if drunk", function() {
         expect(greet("name", true)).to.contain("hello");
         expect(greet("name", true)).to.contain("name");
