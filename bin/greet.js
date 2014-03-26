451433a3405b3efa8f33d984252e557935ef28a6
Durrrr <ihaku4@gmail.com>
2014-03-26 10:33:57 +0800

    coffee-index

--- bin/greet.js
+++ bin/greet.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var greet = require("../index.js");
+var greet = require("../src/index.coffee");
 var parseArgs = require("minimist");
 var name = process.argv[2] || "what's your name?";
 var drunk = parseArgs(process.argv)["drunk"];
